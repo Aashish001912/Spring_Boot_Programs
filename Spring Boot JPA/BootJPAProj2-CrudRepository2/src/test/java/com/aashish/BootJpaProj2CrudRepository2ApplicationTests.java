efef96dfa17c2fa130bfbabcd81ee4457e0072c2
Aashish0019 <aashishprajapati50@gmail.com>
2025-05-16 23:15:53 +0530

    BootJPAProj2-CrudRepository2
    BootJPAProj2-CrudRepository2

---
+++ Spring Boot JPA/BootJPAProj2-CrudRepository2/src/test/java/com/aashish/BootJpaProj2CrudRepository2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aashish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootJpaProj2CrudRepository2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
